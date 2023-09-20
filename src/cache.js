490e525dad7261059a6412d37e8084cba75f0b49
Moberd <ocheretov.ilya@yandex.ru>
2023-09-21 01:43:27 +0300

    lab 1 is done

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,43 @@
 class Cache{
 
+    access_cnt = new Map()
+    vault = new Map()
+
+    constructor(){
+    }
+
+    set(key, value, access=1){
+        if (typeof key === 'number' || typeof key === 'string'){
+            if (typeof access === 'number'){
+                this.vault.set(key, value)
+                this.access_cnt.set(key, access)
+                return true
+            }
+            throw new TypeError("Number of access must be a number")
+        }
+        throw new TypeError("Key must be number or string")
+    }
+
+    get(key){
+        if (this.vault.has(key)) {
+            this.access_cnt.set(key, this.access_cnt.get(key) - 1)
+            let return_value = this.vault.get(key)
+            if (this.access_cnt.get(key) === 0) {
+                this.vault.delete(key)
+                this.access_cnt.delete(key)
+            } 
+            return return_value
+        } 
+        return null
+    }
+    
+    describe(){
+        let res = ''
+        for (let [k, v] of this.vault) {
+            res += `Key:${k} Value:${v} Left entries:${this.access_cnt.get(k)}\n`
+        }
+        return res
+    }
+
 }
 export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,60 @@
 import {Cache} from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
-});
+describe('testing correct set', () => {
+    it("should work", () => {
+        let cache = new Cache()
+        expect(cache.set('test', 'test_value', 3)).toBeTruthy()
+        expect(cache.set(1, 'test_value', 3)).toBeTruthy()
+    })
+    it("using not number for counter should throw error", () => {
+        let cache = new Cache()
+        expect(() => {cache.set('test', 'test_value', 'abcd')}).toThrow(new TypeError("Number of access must be a number"))
+        expect(() => {cache.set('test', 'test_value', [1, 2, 3, 4])}).toThrow(new TypeError("Number of access must be a number"))
+    })
+    it("key must be Sring or Number", () => {
+        let cache = new Cache()
+        expect(() => {cache.set([], 'test_value', 1)}).toThrow(new TypeError("Key must be number or string"))
+        expect(() => {cache.set(undefined, 'test_value', 3)}).toThrow(new TypeError("Key must be number or string"))
+    })
+})
+
+describe('testing get (set is correct)', () => {
+    it('should return monkey', () =>{
+        let cache = new Cache()
+        cache.set('man', 'monkey')
+        expect(cache.get('man')).toBe('monkey')
+    })
+    it('should return null after 1 (default) access', () =>{
+        let cache = new Cache()
+        cache.set('man', 'monkey')
+        cache.get('man')
+        expect(cache.get('man')).toBeNull()
+    })
+    it('should return null after 2 access', () =>{
+        let cache = new Cache()
+        cache.set('test', 'test_value', 2)
+        cache.get('test')
+        cache.get('test')
+        expect(cache.get('test')).toBeNull()
+    })
+    it('should return monkey after rewrite number of access', () =>{
+        let cache = new Cache()
+        cache.set('man', 'monkey')
+        cache.set('man', 'monkey', 2)
+        cache.get('man')
+        expect(cache.get('man')).toBe('monkey')
+    })
+})
+
+describe('testing describing', () => {
+    it('should return empty string', () =>{
+        let cache = new Cache()
+        expect(cache.describe()).toBe('')
+    })
+    it('should return correct string', () =>{
+        let cache = new Cache()
+        cache.set('man', 'monkey', 2)
+        cache.set('test', 'test_value')
+        expect(cache.describe()).toBe('Key:man Value:monkey Left entries:2\nKey:test Value:test_value Left entries:1\n')
+    })
+})
